58df3bd48b46690d2c58e1bd791b3f4923858824
Arshia Farrokhi <136111322+arshiafarrokhi@users.noreply.github.com>
2023-11-21 14:53:11 +0330

    bug fixed

--- assets/js/backToTopButton.js
+++ assets/js/backToTopButton.js
@@ -1,6 +1,11 @@
-let _button = document.getElementById('goToTop')
+let _button1 = document.getElementsByClassName('goToTop')[0]
+let _button2 = document.getElementsByClassName('goToTop')[1]
 
-_button.addEventListener("click" , ()=>{
+_button1.addEventListener("click" , ()=>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+_button2.addEventListener("click" , ()=>{
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 })
