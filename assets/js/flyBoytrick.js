281ba26c7ca19808bd484a13c965a53afb227866
Arshia Farrokhi <136111322+arshiafarrokhi@users.noreply.github.com>
2023-11-23 16:47:11 +0330

    bug fixed

--- assets/js/flyBoytrick.js
+++ assets/js/flyBoytrick.js
@@ -1,9 +1,9 @@
 
 let cursor = document.getElementById("cursor");
 document.addEventListener("mousemove", function (e) {
-    let x = e.pageX / 10;
-    let y = e.pageY / 10 ;
-    cursor.style.top = 200- y + "px";
+    let x = e.pageX / 20;
+    let y = e.pageY / 20 ;
+    cursor.style.top = 10 - y + "px";
     cursor.style.left = -10- x + "px";
 
 
